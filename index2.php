47db5d52b28717c6fff81ebd5db1cfa134348b25
Janani <jananiaugustin08@gmail.com>
2022-02-14 23:44:06 +0530

    Graph view sucess

--- index2.php
+++ index2.php
@@ -1,20 +1,13 @@
 
 <?php
-$con=mysqli_connect("localhost","root","","loginapp");
-if(!$con){
-    echo "Problem in database connection! Contact administrator!" . mysqli_error();
+session_start();
+include "template.php";
+require_once "config.php";
+$view_dt=$view_dt_err=$view_dt_to="";
+$pid=$_SESSION['id'];
 
-}else{
-    $sql="SELECT dt, time_status, waketime from dt_time where id='5'";
-    $result=mysqli_query($con,$sql);
-    $chart_data="";
-    while ($row = mysqli_fetch_array($result)){
-        $dt[]=$row['dt'];
-        $time_status[]=$row['time_status'];
-        $wtime[]=$row['waketime'];
 
-      }
-}
+
 ?>
 
 <!DOCTYPE html>
@@ -28,19 +21,76 @@ if(!$con){
 
 </head>
 <body>
-    <div style="width:30%;height:20%;text-align:center">
-<h2 class="page-header">My Journal</h2>
-<div>Wake Up time</div>
-<canvas id="chartjs_bar"></canvas>
-</div>
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-6">
 
-</body>
+                <?php 
+                    if(isset($_SESSION['status']))
+                    {
+                        ?>
+                            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                            <strong>Hey!</strong> <?php echo $_SESSION['status']; ?>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        <?php
+                         unset($_SESSION['status']);
+                    }
+                ?>
+
+                <div class="card mt-5">
+                              
+                    <div class="card-header">
+                    <a href="graph_home.php" ><p style="text-align:left">Back</p></a> <h4>Graph View</h4>
+                    </div>
+                    <div class="card-body">
+<canvas id="chartjs_bar"></canvas>
+
+
+
 
 
 <script src="//code.jquery.com/jquery-1.9.1.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 
-  
+<?php
+
+                  
+
+
+if (isset($_POST['submit']))
+ {
+
+if ((empty(trim($_POST["view_dt"]))) or (empty(trim($_POST["view_dt_to"])))) {
+$_SESSION['view_dt_err'] = "Please select date.";
+header ("location: graph_home.php");
+
+
+} else {
+$view_dt = trim($_POST["view_dt"]);
+$view_dt_to = trim($_POST["view_dt_to"]);
+}
+
+if (empty($view_dt_err)) {
+//Prepare the SELECT Query
+
+$sql="SELECT dt, time_status, waketime from dt_time where dt BETWEEN '$view_dt' AND '$view_dt_to' and id='$pid' and time_status!='null' order by dt ASC";
+    $result=mysqli_query($link,$sql);
+    $chart_data="";
+    while ($row = mysqli_fetch_array($result)){
+        $dt[]=$row['dt'];
+        $time_status[]=$row['time_status'];
+        $wtime[]=$row['waketime'];
+
+      }
+}
+//Execute the SELECT Query
+}
+if (!($selectRes = mysqli_query($link, $sql))) {
+echo 'No record found';
+} 
+else { 
+?>
 <script type="text/javascript">
  
       var ctx = document.getElementById("chartjs_bar").getContext('2d');
@@ -61,7 +111,11 @@ if(!$con){
                         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero:true
+                    beginAtZero:true,
+                    stepSize:10,
+                    min:0,
+                    max:40
+
                 }
             }]
                         },
@@ -127,4 +181,12 @@ if(!$con){
     });
 
   </script>
+  <?php } ?>
+   </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  </body>
 </html>
